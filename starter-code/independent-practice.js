4feb8a0d856e977465c50c0631a9c30e414b6487
lordshadowist <shadowist@live.com>
2016-09-07 14:56:55 -0700

    Bill Cheng JS Ctrl Flow Lab Finish

--- starter-code/independent-practice.js
+++ starter-code/independent-practice.js
@@ -10,21 +10,24 @@
  * Create an array of image sources. Use image1.png, image2.png, and image3.png as the array values.
  */
 
-// Your code here
+var q1 = ["image1.png", "image2.png", "image3.png"];
+console.log("Question 1" + q1);
 
 /**
  * Question 2
  * Using the array from Question 1, store the first element of the array in variable q2.
  */
 
-// Your code here
+var q2 = q1[0];
+console.log("Question 2" + q2);
 
 /**
  * Question 3
  * Get the length of the first array (number of elements in the array) and store it in variable q3
  */
 
-// Your code here
+var q3 = q1.length;
+console.log("Question 3" + q3);
 
 /**
  * Question 4
@@ -34,7 +37,8 @@
 
 // Your code here
 
-
+var q4 = q1[q1.length - 1];
+console.log("Question 4" + q4);
 // ____________________________________________________________________________
 
 /**
@@ -47,13 +51,24 @@
 The end result should be an array of numbers with values 2,3,4,5
  */
 
-// Your code here
+var q5 = [1,2,3,4];
+for (var i = 0; i < q5.length; i++)
+{
+	q5[i] += 1;
+}
+console.log("Question 5" + q5);
 
 
 /**
  * Question 6
  * Using the array from Question 5, find the average of the numbers in the array (average=sum of all numbers/number of numbers). Store the average in q6.
  */
-
+var q6 = 0;
+for (var i = 0; i < q5.length; i++)
+{
+	q6 += q5[i];
+}
+q6 /= q5.length;
+console.log("Question 6" + q6);
 
 // Your code here
